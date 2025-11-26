bd4d2778cceebf638d1a0903c4694202d55ba8c9
Higor_NTJ <cl205238@g.unicamp.br>
2025-11-26 19:32:16 -0300

    Funções reservar e liberar
    Fiz as funções void reservar e liberar quarto no hotel.h

--- hotel.h
+++ hotel.h
@@ -33,3 +33,55 @@ void listar_quartos(struct Hotel *quartoptr)
         }
     }
 }
+
+
+void reservar_quarto(struct Hotel *quartoptr)
+{
+    int numero=0;
+    printf("Digite o numero do quarto que deseja reservar: ");
+    scanf("%d", &numero);
+    if (numero < 11 && numero > 0)
+    {
+        if (quartoptr[numero-1].status == 0)
+        {
+            quartoptr[numero-1].status = 1;
+            printf("Digite seu nome: ");
+            scanf(" %49[^\n]", quartoptr[numero-1].nome_hospede);
+            printf("Quarto Reservado!\n");
+        }
+        else
+        {
+            printf("O quarto %d ja esta reservado\n", numero);
+        }
+    }
+    else
+    {
+        printf("Numero Invalido!\n");
+    }
+}
+
+void liberar_quarto(struct Hotel *quartoptr)
+{
+    int numero;
+    printf("Digite o quarto que deseja liberar: ");
+    scanf("%d", &numero);
+    if(numero < 11 && numero > 0)
+    {
+        numero=numero-1;
+        if(quartoptr[numero].status == 1)
+        {
+            quartoptr[numero].status=0;
+            strcpy(quartoptr[numero].nome_hospede, " ");
+            printf("Quarto %d liberado com sucesso!\n", numero+1);
+        }
+        else
+        {
+            printf("O quarto %d ja esta vazio!\n", numero+1);
+        }
+    }
+    else
+    {
+        printf("Numero Invalido!\n");
+    }
+    
+}

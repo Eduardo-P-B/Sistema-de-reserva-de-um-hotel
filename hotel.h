56114d23797f12595efd47f4de69f32d52816215
Eduardo-P-B <epiresbatagliotti@gmail.com>
2025-11-26 19:38:18 -0300

    Adicionando mais funções
    Adicionei as funções salvar hotel, carregar hotel e alterar nome do hospede

--- hotel.h
+++ hotel.h
@@ -34,7 +34,6 @@ void listar_quartos(struct Hotel *quartoptr)
     }
 }
 
-
 void reservar_quarto(struct Hotel *quartoptr)
 {
     int numero=0;
@@ -85,3 +84,61 @@ void liberar_quarto(struct Hotel *quartoptr)
     }
     
 }
+
+void alterar_nome(struct Hotel *quartoptr)
+{
+    int numero;
+    printf("Digite o numero do quarto que tera o nome do hospede alterado: ");
+    scanf("%d", &numero);
+    if(numero > 0 && numero < 11)
+    {
+        if (quartoptr[numero-1].status == 1)
+        {
+            numero=numero-1;
+            printf("Digite o nome alterado: ");
+            scanf(" %49[^\n]", quartoptr[numero].nome_hospede);
+            printf("Nome alterado com sucesso!\n");
+        }
+        else
+        {
+            printf("O quarto %d esta vazio!\n", numero);
+        }
+    }
+    else
+    {
+        printf("Numero Invalido!\n");
+    }
+};
+
+void salvar_hotel(struct Hotel *quartoptr)
+{
+    FILE *numeros = fopen("hotel.txt", "w");
+    FILE *hospedes = fopen("hospedes.txt", "w");
+    for(int i=0; i<10; i++)
+    {
+            fprintf(numeros, "%d\n", quartoptr[i].status);
+            fprintf(hospedes, "%s\n", quartoptr[i].nome_hospede);
+    }
+    fclose(numeros);
+    fclose(hospedes);
+    printf("Hotel Salvo!\n");
+}
+
+void carregar_hotel(struct Hotel *quartoptr)
+{
+    int numero=0;
+    FILE *hotel = fopen("hotel.txt", "r");
+    FILE *hospedes = fopen("hospedes.txt", "r");
+    while (fgets(quartoptr[numero].nome_hospede, 50, hospedes) != NULL)
+    {
+        quartoptr[numero].nome_hospede[strcspn(quartoptr[numero].nome_hospede, "\n")] = '\0';
+        numero=numero+1;
+    }
+    numero=0;
+    while (fscanf(hotel, "%d", &quartoptr[numero].status) == 1) {
+        numero=numero+1;
+    }
+    printf("Hotel Carregado!\n");
+}
+
+#endif

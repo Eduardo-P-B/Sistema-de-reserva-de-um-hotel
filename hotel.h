e03982306b4ab67e18420a7c212352a7b5517604
Davi Puzoni <davi.puzoni@outlook.com>
2025-11-26 19:30:01 -0300

    iniciei o hotel.h
    defini a struct, e fiz as funções inicializar_hotel e listar_quartos

--- hotel.h
+++ hotel.h
@@ -1,1 +1,35 @@
+#ifndef HOTEL_H
+#define HOTEL_H
 
+#define NUM_QUARTOS 10
+
+struct Hotel {
+    int numero;
+    int status; // 0 = livre, 1 = ocupado
+    char nome_hospede[50];
+};
+
+void inicializar_hotel(struct Hotel *quartoptr){
+    for(int i = 0; i < NUM_QUARTOS; i++)
+    {
+        quartoptr[i].numero = i+1;
+        quartoptr[i].status = 0;
+        strcpy(quartoptr[i].nome_hospede, " ");
+    }
+}
+
+void listar_quartos(struct Hotel *quartoptr)
+{
+    for(int i=0; i < NUM_QUARTOS; i++)
+    {
+        printf("Quarto %d: ", i+1);
+        if(quartoptr[i].status==0)
+        {
+            printf("Livre\n");
+        }
+        else
+        {
+            printf("Ocupado por: %s\n", quartoptr[i].nome_hospede);
+        }
+    }
+}
